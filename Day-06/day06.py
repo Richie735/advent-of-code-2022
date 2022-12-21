43fb8881ccd8a5544ead34d17a1e0e8579b079c0
Richie735 <Richie735@users.noreply.github.com>
2022-12-21 15:29:56 +0000

    Day-06

---
+++ Day-06/day06.py
@@ -0,0 +1,28 @@
+# Getting the input parameters
+with open("input06.in") as file:
+    input = file.read()
+
+#print(input) 
+
+
+########################
+#        Part 1        #
+########################
+for i in range(4, len(input)):
+    # Divide the different characters
+    marker = set(input[(i-4):i])
+    if len(marker) == 4:    # if size = 4, all characters are different
+        print("Answer to Day 06: \n part-1 => ", i)
+        break
+
+
+########################
+#        Part 2        #
+########################
+# Equal as part 1 but is 14 instead of 4
+for i in range(14, len(input)):
+    # Divide the different characters
+    marker = set(input[(i-14):i])
+    if len(marker) == 14:    # if size = 14, all characters are different
+        print(" part-2 => ", i)
+        break
